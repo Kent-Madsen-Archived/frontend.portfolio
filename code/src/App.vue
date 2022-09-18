fdc269a7b035676693b1a7705676a11d40615840
Madsen, Kent v <kent.vejrup.madsen@designermadsen.dk>
2022-09-18 22:10:09 +0200

    added structure for sass

--- code/src/App.vue
+++ code/src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Home</router-link>
+    <router-link to="/aboutme">About Me</router-link>
+
+    <router-link to=""></router-link>
+    <router-link to=""></router-link>
+
+    <router-link to="login">login</router-link>
+    <router-link to="register">register</router-link>
   </nav>
+
   <router-view/>
 </template>
